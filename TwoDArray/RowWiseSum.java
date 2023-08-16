4ad0fae97f46b052157470f3eceaaa149d8cfb0f
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-16 13:23:13 +0530

    two D arrays

---
+++ TwoDArray/RowWiseSum.java
@@ -0,0 +1,19 @@
+package TwoDArray;
+
+public class RowWiseSum {
+
+    public static void rowSum(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum = sum + arr[i][j];
+            }
+            System.out.println(sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        rowSum(arr);
+    }
+}
