c172fac8a5b48c7a6da23b6d953d1cf3a11a1d0d
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-19 21:04:59 +0530

    power recursion

---
+++ recursion1/CalcPower.java
@@ -0,0 +1,25 @@
+package recursion1;
+
+public class CalcPower {
+
+    public static int power(int x, int n) {
+        /*
+         * Your class should be named Solution
+         * Don't write main().
+         * Don't read input, it is passed as function argument.
+         * Return output and don't print it.
+         * Taking input and printing output is handled automatically.
+         */
+
+        if (n == 0) {
+            return 1;
+        }
+        return x * power(x, n - 1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(3, 4));
+    }
+
+}
