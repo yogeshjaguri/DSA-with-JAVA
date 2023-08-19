bfa581cd3d4ac7b30e01e5b0a8ed62867e140212
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-19 21:06:00 +0530

    num of digitsrecursion

---
+++ TwoDArray/NumOfDigits.java
@@ -0,0 +1,18 @@
+package TwoDArray;
+
+public class NumOfDigits {
+
+    public static int countDigits(int n) {
+        // Write your code here.
+        if (n == 0) {
+            return 0;
+        }
+        return 1 + countDigits(n / 10);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countDigits(12345));
+    }
+
+}
