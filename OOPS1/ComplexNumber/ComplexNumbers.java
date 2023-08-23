6b7162e3128d0a40a40f90569b35bfbe433ff0f9
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-23 09:58:59 +0530

    OOPS Complesnum

---
+++ OOPS1/ComplexNumber/ComplexNumbers.java
@@ -0,0 +1,57 @@
+package OOPS1.ComplexNumber;
+
+import java.util.Scanner;
+
+public class ComplexNumbers {
+    // Complete this class
+    private int real;
+    private int imaginary;
+
+    public ComplexNumbers(int real, int imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public void plus(ComplexNumbers other) {
+        this.real += other.real;
+        this.imaginary += other.imaginary;
+    }
+
+    public void multiply(ComplexNumbers other) {
+        int newReal = this.real * other.real - this.imaginary * other.imaginary;
+        int newImaginary = this.real * other.imaginary + this.imaginary * other.real;
+        this.real = newReal;
+        this.imaginary = newImaginary;
+    }
+
+    public void print() {
+        System.out.println(real + " + i" + imaginary);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int real1 = s.nextInt();
+        int imaginary1 = s.nextInt();
+
+        int real2 = s.nextInt();
+        int imaginary2 = s.nextInt();
+
+        ComplexNumbers c1 = new ComplexNumbers(real1, imaginary1);
+        ComplexNumbers c2 = new ComplexNumbers(real2, imaginary2);
+
+        int choice = s.nextInt();
+
+        if (choice == 1) {
+            // Add
+            c1.plus(c2);
+            c1.print();
+        } else if (choice == 2) {
+            // Multiply
+            c1.multiply(c2);
+            c1.print();
+        } else {
+            return;
+        }
+    }
+}
