e359b277b66dffd717bf39a54e9812209b685f58
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-23 11:15:27 +0530

    OOPS DynamicArray2

---
+++ OOPS1/DynamicArrayClass2/DynamicArrayUse.java
@@ -0,0 +1,25 @@
+package OOPS1.DynamicArrayClass2;
+
+import OOPS1.DynamicArrayClass1.DynamicArray;
+
+public class DynamicArrayUse {
+
+    public static void main(String[] args) {
+        DynamicArray d = new DynamicArray();
+
+        for (int i = 1; i < 100; i++) {
+            d.add(100 + i);
+        }
+        System.out.println(d.size());
+        System.out.println(d.get(2));
+        d.set(3, 170);
+        System.out.println(d.get(3));
+
+        while (!d.isEmpty()) {
+            System.out.println(d.removeLast());
+            System.out.println("size- " + d.size());
+        }
+
+    }
+
+}
