7aa0e072b854eab6d82d660c7964237eed2a6812
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-26 21:26:44 +0530

    stack

---
+++ LinkedList/BracketBalanced.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class BracketBalanced {
+    public static boolean isBalanced(String expression) {
+        // Your code goes here
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : expression.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+}
