820e630851d56fa22ead22fdaaee1178b285ade0
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-28 14:05:57 +0530

    binary trees max height

---
+++ BinaryTrees/MaxHeight.java
@@ -0,0 +1,18 @@
+package BinaryTrees;
+
+import BinaryTrees.Improvement on Tree Input.BinaryTreeNode;
+
+public class MaxHeight {
+    public static int height(BinaryTreeNode<Integer> root) {
+		//Your code goes here
+		if (root == null) {
+            return 0; 
+        }
+        
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        
+        return Math.max(leftHeight, rightHeight) + 1;
+	}
+    
+}
