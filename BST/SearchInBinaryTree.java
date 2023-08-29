90867346f290cde8dce0585dc50871480efe9d2e
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-29 12:46:43 +0530

    SearchInBinaryTrees

---
+++ BST/SearchInBinaryTree.java
@@ -0,0 +1,18 @@
+package BST;
+
+public class SearchInBinaryTree {
+    public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == k) {
+            return true;
+        }
+        if (root.data < k) {
+            return searchInBST(root.right, k);
+        } else {
+            return searchInBST(root.left, k);
+        }
+    }
+
+}
