54bf2d0a695ead18751ba0d969ea4ca023c45bf0
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-29 13:10:47 +0530

    elements b/w k1 & k2

---
+++ BST/ElementsBetweenK1AndK2.java
@@ -0,0 +1,25 @@
+package BST;
+
+import BinaryTrees.Improvement on Tree Input.BinaryTreeNode;
+
+public class ElementsBetweenK1AndK2 {
+
+    public static void elementsInRangeK1K2(BinaryTreeNode<Integer> root,int k1,int k2){
+        if (root == null) {
+            return;
+        }
+
+        if (root.data > k1) {
+            elementsInRangeK1K2(root.left, k1, k2);
+        }
+
+        if (root.data >= k1 && root.data <= k2) {
+            System.out.print(root.data + " ");
+        }
+
+        if (root.data < k2) {
+            elementsInRangeK1K2(root.right, k1, k2);
+        }		
+	 }
+    
+}
