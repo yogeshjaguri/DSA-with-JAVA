eed05915b5d09ccf0245ed46bfc5e61ccf10db58
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-30 10:09:08 +0530

    ReplacewithSumofGreaterNodes

---
+++ BST/ReplacewithSumofGreaterNodes.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class ReplacewithSumofGreaterNodes {
+
+    /*
+     * Binary Tree Node class
+     * 
+     * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T>
+     * right;
+     * 
+     * public BinaryTreeNode(T data) { this.data = data; } }
+     */
+
+    public static void replaceWithLargerNodesSum(BinaryTreeNode<Integer> root) {
+        // Write your code here
+
+        if (root == null) {
+            return;
+        }
+
+        Stack<BinaryTreesreeNode<Integer>> stack = new Stack<>();
+        BinaryTreeNode<Integer> current = root;
+        int runningSum = 0;
+
+        while (!stack.isEmpty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.right;
+            }
+
+            current = stack.pop();
+            current.data += runningSum;
+            runningSum = current.data;
+
+            current = current.left;
+        }
+
+    }
+}
