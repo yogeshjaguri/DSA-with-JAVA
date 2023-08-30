7bf8652eacda481b8717994dc2ff48177c8fb455
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-30 11:47:38 +0530

    BSTCLASS

--- BST/BSTClass.java
+++ BST/BSTClass.java
@@ -45,105 +45,149 @@ public class BSTClass {
      * }
      */
 
-    public class BinarySearchTree {
-
-        private BinaryTreeNode<Integer> root;
-
+     public class BinarySearchTree {
+        // Private instance variable to store the root node of the BST
+        private Node root;
+    
+        // Private inner class representing a node in the BST
+        private class Node {
+            int data;
+            Node left, right;
+    
+            // Constructor to initialize a node with given data
+            Node(int data) {
+                this.data = data;
+            }
+        }
+    
+        // Public method to insert a new element into the BST
         public void insert(int data) {
+            // Update the root after inserting the element
             root = insert(root, data);
         }
-
-        private BinaryTreeNode<Integer> insert(BinaryTreeNode<Integer> root, int data) {
+    
+        // Private recursive method to insert a new element into the BST
+        private Node insert(Node root, int data) {
+            // If the current node is null, create a new node with the given data
             if (root == null) {
-                return new BinaryTreeNode<>(data);
+                return new Node(data);
             }
-
+    
+            // Compare the data with the current node's data and insert accordingly
             if (data <= root.data) {
                 root.left = insert(root.left, data);
             } else {
                 root.right = insert(root.right, data);
             }
-
+    
+            // Return the updated root after insertion
             return root;
         }
-
+    
+        // Public method to remove an element from the BST
         public void remove(int data) {
+            // Update the root after removing the element
             root = remove(root, data);
         }
-
-        private BinaryTreeNode<Integer> remove(BinaryTreeNode<Integer> root, int data) {
+    
+        // Private recursive method to remove an element from the BST
+        private Node remove(Node root, int data) {
+            // If the current node is null, return null
             if (root == null) {
                 return null;
             }
-
+    
+            // Compare the data with the current node's data and remove accordingly
             if (data < root.data) {
                 root.left = remove(root.left, data);
             } else if (data > root.data) {
                 root.right = remove(root.right, data);
             } else {
+                // If the node has both children, replace with the minimum element from the right subtree
                 if (root.left == null) {
                     return root.right;
                 } else if (root.right == null) {
                     return root.left;
                 }
-
+    
+                // Find the minimum value in the right subtree
                 root.data = findMinValue(root.right);
+                // Remove the duplicate value from the right subtree
                 root.right = remove(root.right, root.data);
             }
-
+    
+            // Return the updated root after removal
             return root;
         }
-
-        private int findMinValue(BinaryTreeNode<Integer> root) {
-
+    
+        // Private method to find the minimum value in a subtree
+        private int findMinValue(Node root) {
             while (root.left != null) {
                 root = root.left;
             }
             return root.data;
         }
-
+    
+        // Public method to print the BST in the specified format
         public void printTree() {
+            // Print the tree starting from the root
             printTree(root);
         }
-
-        private void printTree(BinaryTreeNode<Integer> root) {
+    
+        // Private recursive method to print the BST in the specified format
+        private void printTree(Node root) {
+            // If the current node is null, return
             if (root == null) {
                 return;
             }
-
+    
+            // Print the current node's data
             System.out.print(root.data + ":");
-
+    
+            // Print the left child's data if it exists
             if (root.left != null) {
                 System.out.print("L:" + root.left.data + ",");
             }
-
+    
+            // Print the right child's data if it exists
             if (root.right != null) {
                 System.out.print("R:" + root.right.data);
             }
-
+    
+            // Move to the next line
             System.out.println();
-
+    
+            // Recursively print the left and right subtrees
             printTree(root.left);
             printTree(root.right);
         }
-
+    
+        // Public method to search for an element in the BST
         public boolean search(int data) {
+            // Start the search from the root
             return search(root, data);
         }
-
-        private boolean search(BinaryTreeNode<Integer> root, int data) {
+    
+        // Private recursive method to search for an element in the BST
+        private boolean search(Node root, int data) {
+            // If the current node is null, return false
             if (root == null) {
                 return false;
             }
-
+    
+            // Compare the data with the current node's data
             if (root.data == data) {
                 return true;
             } else if (data < root.data) {
+                // Search in the left subtree if the data is smaller
                 return search(root.left, data);
             } else {
+                // Search in the right subtree if the data is larger
                 return search(root.right, data);
             }
         }
+    }
+    
 
     }
 
