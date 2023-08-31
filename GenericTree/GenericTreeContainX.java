a5b1d448ec3e5c362cf1f9e9ed14a351a3f51e31
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-31 18:09:11 +0530

    generic tree contains x

---
+++ GenericTree/GenericTreeContainX.java
@@ -0,0 +1,39 @@
+package GenericTree;
+
+import javax.swing.tree.TreeNode;
+
+public class GenericTreeContainX {
+
+    /*
+     * TreeNode class
+     * 
+     * class TreeNode<T> {
+     * T data;
+     * ArrayList<TreeNode<T>> children;
+     * 
+     * TreeNode(T data){
+     * this.data = data;
+     * children = new ArrayList<TreeNode<T>>();
+     * }
+     * }
+     */
+
+    public static boolean checkIfContainsX(TreeNode<Integer> root, int x) {
+
+        // Write your code here
+        if (root == null) {
+            return false;
+        }
+        if (root.data == x) {
+            return true;
+        }
+        for (TreeNode<Integer> child : root.children) {
+            if (checkIfContainsX(child, x)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
