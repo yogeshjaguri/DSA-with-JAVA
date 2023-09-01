594a29ca6a0536f27203f4fcba87179d51d07d7d
Yogesh <yogesh.jaguri2401@gmail.com>
2023-09-01 17:15:27 +0530

    hashmap implementation rehasging

--- HashMap/HashMap_implementation/Map.java
+++ HashMap/HashMap_implementation/Map.java
@@ -38,7 +38,30 @@ public class Map<K, V> {
         newNode.next = head;
         buckets.set(bucketIndex, newNode);
         count++;
+        Double loadeFactor = (1.0 * count) / numBuckets;
+        if (loadeFactor > 0.7) {
+            rehash();
+        }
 
+    }
+
+    private void rehash() {
+        ArrayList<MapNode<K, V>> temp = buckets;
+        buckets = new ArrayList<>();
+        for (int i = 0; i < 2 * numBuckets; i++) {
+            buckets.add(null);
+        }
+        count = 0;
+        numBuckets = numBuckets * 2;
+        for (int i = 0; i < temp.size(); i++) {
+            MapNode<K, V> head = temp.get(i);
+            while (head != null) {
+                K key = head.key;
+                V value = head.value;
+                insert(key, value);
+                head = head.next;
+            }
+        }
     }
 
     public V getValue(K key) {
