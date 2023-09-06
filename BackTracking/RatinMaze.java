c5d04662d775eb4721c352e0004a4915a44e6537
Yogesh <yogesh.jaguri2401@gmail.com>
2023-09-06 13:36:54 +0530

    Rat in Maze all possible sol

--- BackTracking/RatinMaze.java
+++ BackTracking/RatinMaze.java
@@ -4,55 +4,106 @@ import java.util.Scanner;
 
 public class RatinMaze {
 
-    public static boolean ratInAMaze(int maze[][]) {
+    // public static boolean ratInAMaze(int maze[][]) {
 
+    // /*
+    // * Your class should be named Solution.
+    // * Don't write main().
+    // * Don't take input, it is passed as function argument.
+    // * Don't print output.
+    // * Taking input and printing output is handled automatically.
+    // */
+    // int n = maze.length;
+    // int path[][] = new int[n][n];
+    // return solveMaze(maze, 0, 0, path);
+    // }
+
+    // private static boolean isValidMove(int maze[][], int i, int j, int path[][])
+    // {
+    // int n = maze.length;
+    // return i >= 0 && i < n && j >= 0 && j < n && maze[i][j] == 1 && path[i][j] ==
+    // 0;
+    // }
+
+    // private static boolean solveMaze(int maze[][], int i, int j, int path[][]) {
+    // int n = maze.length;
+    // if (isValidMove(maze, i, j, path)) {
+    // path[i][j] = 1; // Mark the cell as visited
+
+    // if (i == n - 1 && j == n - 1) {
+    // return true;
+    // }
+
+    // if (solveMaze(maze, i + 1, j, path) || // Down
+    // solveMaze(maze, i - 1, j, path) || // Up
+    // solveMaze(maze, i, j + 1, path) || // Right
+    // solveMaze(maze, i, j - 1, path)) { // Left
+    // return true;
+    // }
+    // }
+
+    // // if(SolveMaze(maze, i-1, j, path)){
+    // // return true;
+    // // }
+    // // if(SolveMaze(maze, i, j + 1, path)){
+    // // return true;
+    // // }
+    // // if(SolveMaze(maze, i +1, j, path)){
+    // // return true;
+    // // }
+    // // if(SolveMaze(maze, i, j - 1, path)){
+    // // return true;
+    // // }
+    // return false;
+    // }
+
+    static void ratInAMaze(int maze[][], int n) {
         /*
          * Your class should be named Solution.
-         * Don't write main().
-         * Don't take input, it is passed as function argument.
-         * Don't print output.
-         * Taking input and printing output is handled automatically.
+         * Write your code here
          */
-        int n = maze.length;
         int path[][] = new int[n][n];
-        return solveMaze(maze, 0, 0, path);
+        printAllPaths(maze, n, 0, 0, path);
     }
 
-    private static boolean isValidMove(int maze[][], int i, int j, int path[][]) {
-        int n = maze.length;
+    private static boolean isValidMove(int maze[][], int i, int j, int path[][], int n) {
         return i >= 0 && i < n && j >= 0 && j < n && maze[i][j] == 1 && path[i][j] == 0;
     }
 
-    private static boolean solveMaze(int maze[][], int i, int j, int path[][]) {
-        int n = maze.length;
-        if (isValidMove(maze, i, j, path)) {
-            path[i][j] = 1; // Mark the cell as visited
-
-            if (i == n - 1 && j == n - 1) {
-                return true;
-            }
-
-            if (solveMaze(maze, i + 1, j, path) || // Down
-                    solveMaze(maze, i - 1, j, path) || // Up
-                    solveMaze(maze, i, j + 1, path) || // Right
-                    solveMaze(maze, i, j - 1, path)) { // Left
-                return true;
-            }
+    private static void printAllPaths(int maze[][], int n, int i, int j, int path[][]) {
+        if (i == n - 1 && j == n - 1) {
+            path[i][j] = 1;
+            printPath(path, n);
+            path[i][j] = 0;
+            return;
         }
 
-        // if(SolveMaze(maze, i-1, j, path)){
-        // return true;
-        // }
-        // if(SolveMaze(maze, i, j + 1, path)){
-        // return true;
-        // }
-        // if(SolveMaze(maze, i +1, j, path)){
-        // return true;
-        // }
-        // if(SolveMaze(maze, i, j - 1, path)){
-        // return true;
-        // }
-        return false;
+        if (isValidMove(maze, i, j, path, n)) {
+            path[i][j] = 1;
+
+            // Move up
+            printAllPaths(maze, n, i - 1, j, path);
+
+            // Move down
+            printAllPaths(maze, n, i + 1, j, path);
+
+            // Move left
+            printAllPaths(maze, n, i, j - 1, path);
+
+            // Move right
+            printAllPaths(maze, n, i, j + 1, path);
+
+            path[i][j] = 0;
+        }
+    }
+
+    private static void printPath(int[][] path, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(path[i][j] + " ");
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -64,7 +115,7 @@ public class RatinMaze {
                 maze[i][j] = s.nextInt();
             }
         }
-        System.out.println(RatinMaze.ratInAMaze(maze));
+        RatinMaze.ratInAMaze(maze, n);
     }
 
 }
