87ee2e936b70eddbf4561489883528d8a245fbb6
Yogesh <yogesh.jaguri2401@gmail.com>
2023-09-10 11:53:07 +0530

    min cost to destination dp

--- HashMap/MinCostToReachDestination.java
+++ HashMap/MinCostToReachDestination.java
@@ -6,22 +6,47 @@ import java.io.InputStreamReader;
 
 public class MinCostToReachDestination {
     public static int minCostPath(int[][] input) {
-        return minCostRecursive(input, 0, 0);
-    }
+        int rows = input.length;
+        int cols = input[0].length;
 
-    private static int minCostRecursive(int[][] cost, int i, int j) {
-        int rows = cost.length;
-        int cols = cost[0].length;
+        int[][] dp = new int[rows][cols];
+        dp[0][0] = input[0][0];
 
-        if (i == rows - 1 && j == cols - 1) {
-            return cost[i][j];
+        for (int j = 1; j < cols; j++) {
+            dp[0][j] = dp[0][j - 1] + input[0][j];
         }
 
-        int rightCost = (j < cols - 1) ? minCostRecursive(cost, i, j + 1) : Integer.MAX_VALUE;
-        int downCost = (i < rows - 1) ? minCostRecursive(cost, i + 1, j) : Integer.MAX_VALUE;
-        int diagonalCost = (i < rows - 1 && j < cols - 1) ? minCostRecursive(cost, i + 1, j + 1) : Integer.MAX_VALUE;
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + input[i][0];
+        }
 
-        return cost[i][j] + Math.min(Math.min(rightCost, downCost), diagonalCost);
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = input[i][j] + Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+            }
+        }
+
+        return dp[rows - 1][cols - 1];
+
+        // return minCostRecursive(input, 0, 0);
+        // }
+
+        // private static int minCostRecursive(int[][] cost, int i, int j) {
+        // int rows = cost.length;
+        // int cols = cost[0].length;
+
+        // if (i == rows - 1 && j == cols - 1) {
+        // return cost[i][j];
+        // }
+
+        // int rightCost = (j < cols - 1) ? minCostRecursive(cost, i, j + 1) :
+        // Integer.MAX_VALUE;
+        // int downCost = (i < rows - 1) ? minCostRecursive(cost, i + 1, j) :
+        // Integer.MAX_VALUE;
+        // int diagonalCost = (i < rows - 1 && j < cols - 1) ? minCostRecursive(cost, i
+        // + 1, j + 1) : Integer.MAX_VALUE;
+
+        // return cost[i][j] + Math.min(Math.min(rightCost, downCost), diagonalCost);
     }
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
